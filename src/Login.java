f9d9aec5f9a8cac5a110d442784a5183159c6b6b
Milena Runes <milena.runets@gmail.com>
2024-10-17 20:06:06 +0200

    feat: login

--- src/Login.java
+++ src/Login.java
@@ -8,7 +8,7 @@ public class Login {
         System.out.print("Enter username: ");
         String username = scanner.nextLine();
         System.out.print("Enter password: ");
-        //comment
+        //comment 1
         String password = scanner.nextLine();
 
         if (usersDatabase.containsKey(username) && usersDatabase.get(username).equals(password)) {
